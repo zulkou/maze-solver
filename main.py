9729f376598653147175edfbe406eb5a16fdfdf3
zulkou <73563319+zulkou@users.noreply.github.com>
2025-02-02 07:58:22 +0700

    feat: add draw line

--- main.py
+++ main.py
@@ -27,9 +27,37 @@ class Window:
     def close(self):
         self.is_running = False
 
+    def draw_line(self, line, fill_color):
+        line.draw(self.canvas, fill_color)
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+
+class Line:
+    def __init__(self, point1, point2):
+        self.point1 = point1
+        self.point2 = point2
+
+    def draw(self, canvas, fill_color):
+        canvas.create_line(
+            self.point1.x,
+            self.point1.y,
+            self.point2.x,
+            self.point2.y,
+            fill=fill_color,
+            width=2,
+        )
+
 
 def main():
     win = Window(800, 600)
+    point1 = Point(200, 300)
+    point2 = Point(600, 300)
+    line = Line(point1, point2)
+    win.draw_line(line, "black")
     win.wait_for_close()
 
 
